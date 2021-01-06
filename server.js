54fb9e99cda4a9d3785fd22d894aa20b439293b3
clintonstrange <71712425+clintonstrange@users.noreply.github.com>
2021-01-05 20:38:15 -0600

    added role and added employee

--- server.js
+++ server.js
@@ -152,7 +152,7 @@ const viewAllEmployees = () => {
           `=====================================================================================`
         )
       );
-      console.log(chalk.yello(`ALL EMPLOYEES`));
+      console.log(chalk.yellow(`ALL EMPLOYEES`));
       console.log(
         chalk.magenta(
           `=====================================================================================`
@@ -298,22 +298,151 @@ const addRole = () => {
       },
     ])
     .then((answer) => {
-      console.log(answer);
       const departmentId = selectDepartment().indexOf(answer.department) + 1;
-      const newRole = {
-        title: answer.title,
-        salary: answer.salary,
-        department_id: departmentId,
-      };
-      console.log(newRole);
+      const newRole = [answer.title, answer.salary, departmentId.toString()];
       connection.query(
-        "INSERT INTO role (title, salary, department_id) VALUES ?, ?, ?",
-        {newRole.Title},
-        function (error) {
+        `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`,
+        newRole,
+        (error) => {
           if (error) throw error;
-          console.table(newRole);
+          console.log(
+            chalk.magenta(
+              `=====================================================================================`
+            )
+          );
+          console.log(chalk.yellow(`ADDED ROLE: ` + answer.title));
+          console.log(
+            chalk.magenta(
+              `=====================================================================================`
+            )
+          );
           viewAllRoles();
         }
       );
+    });
+};
+
+let roleArr = [];
+function selectRole() {
+  connection.query("SELECT * FROM role", function (error, response) {
+    if (error) throw error;
+    for (var i = 0; i < response.length; i++) {
+      roleArr.push(response[i].title);
+    }
+  });
+  return roleArr;
+}
+
+// let managerArr = [];
+// function selectManager() {
+//   connection.query(
+//     "SELECT first_name, last_name FROM employee WHERE manager_id IS NULL",
+//     function (error, response) {
+//       if (error) throw error;
+//       for (var i = 0; i < response.length; i++) {
+//         managerArr.push(response[i].first_name + " " + response[i].last_name);
+//       }
+//     }
+//   );
+//   return managerArr;
+// }
+
+const addEmployee = () => {
+  inquirer
+    .prompt([
+      {
+        name: "first_name",
+        type: "input",
+        message: "What is the first name of the employee you are adding?",
+        validate: (firstInput) => {
+          if (firstInput) {
+            return true;
+          } else {
+            console.log(
+              "Please provide the first name of the employee you are adding."
+            );
+            return false;
+          }
+        },
+      },
+      {
+        name: "last_name",
+        type: "input",
+        message: "What is the last name of the employee you are adding?",
+        validate: (lastInput) => {
+          if (lastInput) {
+            return true;
+          } else {
+            console.log(
+              "Please provide the last name of the employee you are adding."
+            );
+            return false;
+          }
+        },
+      },
+      {
+        name: "role_id",
+        type: "list",
+        message: "What is the role of this employee?",
+        choices: selectRole(),
+      },
+    ])
+    .then((answer) => {
+      console.log(answer);
+      const roleId = selectRole().indexOf(answer.role_id) + 1;
+      // const managerId = selectManager().indexOf(answer.manager_id) + 1;
+      const newEmployee = [
+        answer.first_name,
+        answer.last_name,
+        roleId.toString(),
+        // managerId.toString(),
+      ];
+      console.log(newEmployee);
+      connection.query(`SELECT * FROM employee`, (error, data) => {
+        if (error) throw error;
+        const managers = data.map(({ id, first_name, last_name }) => ({
+          name: first_name + " " + last_name,
+          value: id,
+        }));
+        inquirer
+          .prompt([
+            {
+              name: "manager_id",
+              type: "list",
+              message: "Who is this employee's manager?",
+              choices: managers,
+            },
+          ])
+          .then((managerSelected) => {
+            const manager = managerSelected.manager_id;
+            newEmployee.push(manager);
+            connection.query(
+              `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`,
+              newEmployee,
+              (error) => {
+                if (error) throw error;
+                console.log(
+                  chalk.magenta(
+                    `=====================================================================================`
+                  )
+                );
+                console.log(
+                  chalk.yellow(
+                    `ADDED EMPLOYEE: ` +
+                      answer.first_name +
+                      ` ` +
+                      answer.last_name
+                  )
+                );
+                console.log(
+                  chalk.magenta(
+                    `=====================================================================================`
+                  )
+                );
+                viewAllEmployees();
+              }
+            );
+          });
+      });
     });
 };
